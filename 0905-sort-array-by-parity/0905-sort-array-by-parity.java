9a305812a1634fad17a43d7416c2488c93084efc
rajyadav1503 <raajyadav1503@gmail.com>
2026-07-04 08:54:46 +0530

    Time: 0 ms (100%), Space: 46.8 MB (68.51%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,21 +1,16 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        int a = 0;
+        int left = 0;
         for(int i=0;i<n;i++){
-            if(nums[i] % 2==0){
-                ans[a]=nums[i];
-                a++;
-            }
-            
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]%2 != 0){
-                ans[a]=nums[i];
-                a++;
+            if(nums[i] % 2 == 0){
+                int temp = nums[left];
+                nums[left] = nums[i];
+                nums[i] = temp;
+                left++;
             }
         }
-        return ans;
+        return nums;
+        
     }
 }
