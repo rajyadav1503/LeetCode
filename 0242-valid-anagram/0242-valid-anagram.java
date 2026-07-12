1a509597d50864bc09daf525ab139d7d01c9417f
rajyadav1503 <raajyadav1503@gmail.com>
2026-07-12 21:26:56 +0530

    Time: 6 ms (43.42%), Space: 46.5 MB (34.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,39 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-         if (s.length() != t.length()) return false;
+        //  if (s.length() != t.length()) return false;
 
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+        // int[] freq = new int[26];
+        // for (int i = 0; i < s.length(); i++) {
+        //     freq[s.charAt(i) - 'a']++;
+        //     freq[t.charAt(i) - 'a']--;
+        // }
+
+        // for (int count : freq) {
+        //     if (count != 0) return false;
+        // }
+
+        // return true;
+
+        int n = s.length();
+        if (s.length() != t.length()) return false;
+        char[] arr = new char[s.length()];
+        char[] arr1 = new char[s.length()];
+
+
+
+        for(int i=0;i<n;i++){
+            arr[i]=s.charAt(i);
         }
 
-        for (int count : freq) {
-            if (count != 0) return false;
+        Arrays.sort(arr);
+
+         for(int i=0;i<n;i++){
+            arr1[i]=t.charAt(i);
         }
+        Arrays.sort(arr1);
 
-        return true;
+         return Arrays.equals(arr,arr1);
 
-        
+           
     }
 }
