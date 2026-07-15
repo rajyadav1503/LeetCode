21be6a66db72ce4989551ab80947e0db051296e7
rajyadav1503 <raajyadav1503@gmail.com>
2026-07-15 08:38:15 +0530

    Time: 4 ms (99.33%), Space: 44.4 MB (8.41%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,21 +1,40 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
 
+    public static String createMapping(String s){
         int n = s.length();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
+        char[] arr = new char[256];
+        char start = 'a';
 
-                if(s.charAt(i) == s.charAt(j) &&
-                   t.charAt(i) != t.charAt(j))
-                    return false;
-
-                if(t.charAt(i) == t.charAt(j) &&
-                   s.charAt(i) != s.charAt(j))
-                    return false;
+        //create mapping 
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(arr[ch] == 0){
+                arr[ch]=start;
+                start++;
             }
         }
 
+        //build answer
+        StringBuilder ans = new StringBuilder();
+
+        for(int i=0;i<n;i++){
+            ans.append(arr[s.charAt(i)]);
+        }
+
+        return ans.toString();
+    }
+
+
+    public boolean isIsomorphic(String s, String t) {
+
+       String str1 = createMapping(s);
+       String str2 = createMapping(t);
+
+       if(str1.equals(str2)){
         return true;
+       }
+
+       return false;
     }
 }
