a8370931d24efee8e2216976ed977b4da1d83dce
rajyadav1503 <raajyadav1503@gmail.com>
2026-07-31 13:41:51 +0530

    Time: 2 ms (98.93%), Space: 43.3 MB (85.15%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -1,28 +1,26 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
 
-        boolean[] isBroken = new boolean[26];
+        int[] broken = new int [26];
         for(char ch : brokenLetters.toCharArray()) {
-            isBroken[ch - 'a'] = true;
-        }
 
+            broken [ch - 'a']++;
+        }
         String[] words = text.split(" ");
         int count = 0;
-
-        for(String word : words ){
-            boolean isTyped = true;
-            for(char ch : word.toCharArray()){
-                if(isBroken[ch -'a']){
-                    isTyped = false;
-                    break;
-                }
-            }
-
-            if(isTyped) {
-                count++;
+        for(String word: words){
+        int i;
+        for ( i = 0 ; i < word.length(); i++){
+            if (broken [word.charAt(i) -'a'] > 0) {
+                break;
             }
         }
+        if(i== word.length()) {
+            count++;
+        }
+    }
 
-        return count;
+    return count;
+
     }
 }
