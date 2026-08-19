ef2f7799ccec35809f9f7cdf145cb14ae7dc3145
rajyadav1503 <raajyadav1503@gmail.com>
2026-08-19 08:23:28 +0530

    Time: 4 ms (3.45%), Space: 43.5 MB (12.01%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,18 +1,6 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        String s1 = word.toUpperCase();
-        String s2 = word.toLowerCase();
-        String s3 = "";
-        s3 += s1.charAt(0);
-        for(int i=1;i<s2.length();i++){
-            s3 += s2.charAt(i);
-        }
 
-        if(word.equals(s1) || word.equals(s2) || word.equals(s3)){
-            return true;
-        }
-
-        return false;
-        
+        return (word.equals(word.toUpperCase()) || word.equals(word.toLowerCase()) || word.equals(Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase())); 
     }
 }
